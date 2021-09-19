629a41970f359032fee37d8346b84a59ae64abbc
Isantanna <igor_sant@hotmail.com>
2021-09-19 16:53:47 -0300

    Build Test Launched
    Movie BG fixed

---
+++ Assets/Scripts/VideoScript.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Video;
+
+public class VideoScript : MonoBehaviour
+{
+    public VideoPlayer Bg;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        Bg = GetComponent<VideoPlayer>();
+        Bg.url = System.IO.Path.Combine(Application.streamingAssetsPath, "Bg.mp4");
+        Bg.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
